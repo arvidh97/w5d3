4f08f324e5fbd5805646626aece54484c69e8049
lewiskk016 <lewiskk016@gmail.com>
2023-04-05 10:18:52 -0400

    create replies

--- import_db.sql
+++ import_db.sql
@@ -1,17 +1,31 @@
 CREATE TABLE users(
-    id INTEGER PRIMARY KEY
-    fname VARCHAR(255)
-    lname VARCHAR(255)
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
 CREATE TABLE questions(
-    id INTEGER PRIMARY KEY
-    title VARCHAR(255)
-    body TEXT 
-    author INTEGER NOT NULL 
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    body TEXT,
+    author INTEGER NOT NULL,
 
-    FOREIGN KEY (author) REFRENCES users(id)
+    FOREIGN KEY (author) References users(id)
 );
 
-CREATE TABLE questions_follows 
-    
+CREATE TABLE questions_follows(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    questions_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) References user(id),
+    FOREIGN KEY (questions_id) References questions(id)
+);
+
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL REFERENCES questions(id),
+    parent_id INTEGER REFERENCES replies(id),
+    user_id INTEGER REFERENCES users(id),
+    body TEXT
+);
