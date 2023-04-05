76dd0768760c43e78fea2f741f5fbfd510d41b48
lewiskk016 <lewiskk016@gmail.com>
2023-04-05 11:21:15 -0400

    populating

--- import_db.sql
+++ import_db.sql
@@ -20,7 +20,7 @@ CREATE TABLE questions_follows(
     user_id INTEGER NOT NULL,
     questions_id INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) References user(id),
+    FOREIGN KEY (user_id) References users(id),
     FOREIGN KEY (questions_id) References questions(id)
 );
 
@@ -29,15 +29,36 @@ CREATE TABLE replies(
     question_id INTEGER NOT NULL REFERENCES questions(id),
     parent_id INTEGER REFERENCES replies(id),
     user_id INTEGER NOT NULL REFERENCES users(id),
-    body TEXT NOT NULL 
+    body TEXT NOT NULL
 );
 
 CREATE TABLE question_likes(
     user_id INTEGER NOT NULL REFERENCES users(id),
     question_id INTEGER NOT NULL REFERENCES questions(id),
     PRIMARY KEY (user_id, question_id)
-    -- (user_id, question_id) PRIMARY KEY 
+    -- (user_id, question_id) PRIMARY KEY
 );
 
 
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Steve', 'Steven'),
+    ('Pete', 'Peterson'),
+    ('Dave', 'Davidson'),
+    ('Brad', 'Bradford');
 
+INSERT INTO
+    questions (title, body, author)
+VALUES
+    ('sql?', 'How does any of this work?', (SELECT id FROM users WHERE lname = 'Bradford'));
+
+INSERT INTO
+    questions_follows (user_id, questions_id)
+VALUES
+    ((SELECT id from users WHERE lname = 'Bradford'), (SELECT id from questions WHERE id = 1));
+
+-- INSERT INTO
+--     replies (question_id, parent_id, user_id, body)
+-- VALUES
+--     ()
