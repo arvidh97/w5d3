cbce627a265555192a014dea1c200a3a99fbb9c1
Arvid Hossain <arvidh97@gmail.com>
2023-04-05 11:45:10 -0400

    created test data for tables

--- import_db.sql
+++ import_db.sql
@@ -51,14 +51,27 @@ VALUES
 INSERT INTO
     questions (title, body, author)
 VALUES
-    ('sql?', 'How does any of this work?', (SELECT id FROM users WHERE lname = 'Bradford'));
+    ('sql?', 'How does any of this work?', (SELECT id FROM users WHERE lname = 'Bradford')),
+    ("app academy", "Is this any good?", (SELECT id FROM users WHERE lname = 'Peterson')),
+    ("help", "can i go to the bathroom?", (SELECT id FROM users WHERE lname= 'Davidson')),
+    ("weather", "does anyone know the weather?", (SELECT id FROM users WHERE lname= 'Steven'));
 
 INSERT INTO
     questions_follows (user_id, questions_id)
 VALUES
-    ((SELECT id from users WHERE lname = 'Bradford'), (SELECT id from questions WHERE id = 1));
+    ((SELECT id from users WHERE lname = 'Bradford'), (SELECT id from questions WHERE id = 1)),
+    ((SELECT id from users WHERE lname = 'Peterson'), (SELECT id from questions WHERE id = 2)),
+    ((SELECT id from users WHERE lname = 'Davidson'), (SELECT id from questions WHERE id = 3)),
+    ((SELECT id from users WHERE lname = 'Steven'), (SELECT id from questions WHERE id = 4));
 
--- INSERT INTO
---     replies (question_id, parent_id, user_id, body)
--- VALUES
---     ()
+INSERT INTO
+    replies (question_id, parent_id, user_id, body)
+VALUES
+    ((SELECT questions_id FROM questions_follows WHERE id = 1), NULL, (SELECT id FROM users WHERE id = 2), "I don't know either"),
+    ((SELECT questions_id FROM questions_follows WHERE id = 2), NULL, (SELECT id FROM users WHERE id = 3), "It's awfully bad"),
+    ((SELECT questions_id FROM questions_follows WHERE id = 3), NULL, (SELECT id FROM users WHERE id = 4), "Bathroom's full"),
+    ((SELECT questions_id FROM questions_follows WHERE id = 4), NULL, (SELECT id FROM users WHERE id = 1), "it's nice out"),
+    ((SELECT questions_id FROM questions_follows WHERE id = 1), 1, (SELECT id FROM users WHERE id = 4), "that helped a lot");
+
+
+
